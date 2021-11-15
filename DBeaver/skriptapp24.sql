52273cbc0236789c2e431d978770e1adb6888285
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-15 14:23:06 +0100

    skriptajp24

---
+++ DBeaver/skriptapp24.sql
@@ -0,0 +1,58 @@
+drop database if exists edunovapp24;
+create database edunovapp24;
+use edunovapp24;
+
+drop database if exists edunovapp24;
+create database edunovapp24;
+use edunovapp24;
+
+create table smjer(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    trajanje int not null,
+    cijena decimal(18,2),
+    certifikat boolean
+);
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    email varchar(50)
+);
+
+create table grupa(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    smjer int not null,
+    predavac int,
+    datumpocetka datetime
+);
+
+create table predavac(
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    iban varchar(50)
+);
+
+create table polaznik(
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    brojugovora varchar(50) not null
+);
+
+create table clan(
+    grupa int not null,
+    polaznik int not null
+);
+
+alter table grupa add foreign key (smjer) references nsmjer(sifra);
+alter table grupa add foreign key (predavac) references predavac(sifra);
+
+alter table clan add foreign key (grupa) references grupa(sifra);
+alter table clan add foreign key (polaznik) references polaznik(sifra);
+
+alter table polaznik add foreign key (osoba) references osoba(sifra);
+
+alter table predavac add foreign key (osoba) references osoba(sifra);
