3e65e035068ff31d1b68f45dd71480c8f35d36b8
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-15 14:24:11 +0100

    firma

---
+++ DBeaver/firmapp24.sql
@@ -0,0 +1,26 @@
+drop database if exists firmapp24;
+create database firmapp24;
+use firmapp24;
+
+create table projekt(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal(18,2)
+);
+
+create table programer(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    datumrodenja datetime 
+);
+
+create table sudjeluje(
+    projekt int not null,
+    programer int not null,
+    datumpocetka datetime,
+    datumkraja datetime
+);
+
+alter table sudjeluje add foreign key (programer) references programer(sifra);
+alter table sudjeluje add foreign key (projekt) references projekt(sifra);
