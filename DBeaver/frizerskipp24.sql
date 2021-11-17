5dc321dfd35f3a7cdd386e7c7a289a3c28096b9e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-17 14:30:19 +0100

    insert

--- DBeaver/frizerskipp24.sql
+++ DBeaver/frizerskipp24.sql
@@ -34,3 +34,27 @@ alter table salon add foreign key (djelatnica) references djelatnica (sifra);
 alter table djelatnica add foreign key (korisnik) references korisnik (sifra);
 
 alter table korisnik add foreign key (usluga) references usluga (sifra);
+
+
+
+select * from usluga;
+insert into usluga(sifra,naziv,cijena)
+values (1,'Usluga01',99.99),
+       (2,'Usluga01',199.99),
+       (3,'Usluga01',299.99),
+       (4,'Usluga01',399.99),
+       (5,'Usluga01',499.99);
+
+select * from korisnik;
+insert into korisnik(sifra,ime,prezime,usluga)
+values
+
+
+select * from djelatnica;
+insert into djelatnica(sifra,ime,prezime,korisnik)
+values
+
+
+select * from salon;
+insert into salon(sifra,naziv,djelatnica)
+values
