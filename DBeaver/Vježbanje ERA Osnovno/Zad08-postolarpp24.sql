bc04c51b5d946a5dee5e3f64bd7fe745dfa1f15f
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-19 17:20:57 +0100

    zad08

---
+++ DBeaver/Vježbanje ERA Osnovno/Zad08-postolarpp24.sql
@@ -0,0 +1,44 @@
+drop database if exists postolarp24;
+create database postolarp24;
+use postolarp24;
+
+create table postolar (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    segrt int not null
+);
+
+create table korisnik (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    obuca int not null
+);
+
+create table obuca (
+    sifra int not null primary key auto_increment,
+    nazivMarke varchar(50),
+    popravak int not null
+);
+
+create table popravak (
+    sifra int not null primary key auto_increment,
+    datumPopravka datetime
+);
+
+create table segrt (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    popravak int not null
+);
+
+
+alter table korisnik add foreign key(obuca) references obuca(sifra);
+
+alter table obuca add foreign key(popravak) references popravak(sifra);
+
+alter table segrt add foreign key(popravak) references popravak(sifra);
+
+alter table postolar add foreign key(segrt) references segrt(sifra);
