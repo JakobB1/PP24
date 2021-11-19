4fbfbc8d37b964b5c47482263e7d4cf1afb6fa1e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-11-19 15:00:27 +0100

    muzej zadatak

---
+++ DBeaver/Vježbanje ERA Osnovno/muzejpp24.sql
@@ -0,0 +1,42 @@
+drop database if exists muzejpp24;
+create database muzejpp24;
+use muzejpp24;
+
+create table muzej (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    izlozba int not null
+);
+
+create table izlozba ( 
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    djela int not null,
+    sponzor int not null
+);
+
+create table djela (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50)
+);
+
+create table kustos (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    izlozba int not null
+);
+
+create table sponzor (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50)
+);
+
+
+alter table muzej add foreign key(izlozba) references izlozba(sifra);
+
+alter table kustos add foreign key(izlozba) references izlozba(sifra);
+
+alter table izlozba add foreign key(djela) references djela(sifra);
+alter table izlozba add foreign key(sponzor) references sponzor(sifra);
