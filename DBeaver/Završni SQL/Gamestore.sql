1ff7a0ad499455fb4cea98e00a3ae3c6182fffde
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-12-01 14:51:46 +0100

    Inserti

--- DBeaver/Završni SQL/Gamestore.sql
+++ DBeaver/Završni SQL/Gamestore.sql
@@ -21,11 +21,11 @@ create table games (
     developers int,
     publishers int,
     genre varchar(50),
-    price int,
+    price decimal(15,2),
     review int,
     age_limit int,
     release_date varchar(50),
-    description varchar(50)
+    description varchar(300)
 );
 
 create table users (
@@ -33,7 +33,7 @@ create table users (
     name varchar(50),
     surname varchar(50),
     username varchar (50),
-    password int,
+    password bigint(100),
     gender varchar(50),
     age int,
     email varchar(50),
@@ -74,3 +74,49 @@ alter table orders add foreign key (games) references games(game_id);
 alter table orders add foreign key (users) references users(user_id);
 
 alter table payments add foreign key (users) references users(user_id);
+
+
+
+
+insert into developers (developer_id,name,website) values 
+(1, 'FromSoftware Inc.', 'https://www.fromsoftware.jp/ww/');
+select * from developers;
+
+
+
+insert into publishers (publisher_id,name,country,website) values 
+(1, 'BANDAI NAMCO', 'Japan', 'https://www.bandainamcoent.co.jp/english/');
+select * from publishers;
+
+
+
+insert into games (game_id,name,developers,publishers,genre,price,review,age_limit,release_date,description) values 
+(1, 'Dark Souls', 1, 1, 'Action RPG, Fantasy, Multiplayer, Co-op', 19.99, 9, 18, '22-09-2011', 'Dark Souls takes place in the fictional kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.');
+select * from games;
+
+
+
+insert into users (user_id,name,surname,username,password,gender,age,email,country) values 
+(1, 'NameExample', 'SurnameExample', 'UsernameExample', 123456, 'GenderExample', 20, 'email@example.com', 'CountryExample');
+select * from users;
+
+
+
+insert into orders (order_id,users,games,price,payment_id,`date`) values
+(1, 1, 1, 19.99, 28, '2021-12-01 11:59:00');
+
+select * from orders;
+
+
+
+insert into wishlists (wish_id,users,games,`date`) values
+(1, 1, 1, '2021-12-01 11:59:00');
+
+select * from wishlists;
+
+
+
+insert into payments (payment_id,users,payment_type,card_number,valid) values
+(1, 1, 'Visa', 4539791458727308, true);
+
+select * from payments;
