88ed8089229a242ff1058bb1483ba21ddb911f8e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-03-21 12:40:09 +0100

    predavanje 28

--- EdunovaPP24/app/controller/SmjerController.php
+++ EdunovaPP24/app/controller/SmjerController.php
@@ -12,8 +12,7 @@ class SmjerController extends AutorizacijaController
     {
         // print_r(Smjer::read());
         $this->view->render($this->viewDir . 'index',[
-            'smjerovi' => Smjer::read(),
-            'css'=>'<link rel="stylesheet" href="' . App::config('url') . 'public/css/smjerindex.css">'
+            'smjerovi' => Smjer::read()
         ]);
     }
 }
