8df67379ffdc3f8b3bb1a54e7985138cdbbd3228
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-03-23 15:17:20 +0100

    Nastavak

--- EdunovaPP24/app/controller/PredavacController.php
+++ EdunovaPP24/app/controller/PredavacController.php
@@ -7,10 +7,119 @@ class PredavacController extends AutorizacijaController
                 'privatno' . DIRECTORY_SEPARATOR . 
                     'predavaci' . DIRECTORY_SEPARATOR;
 
+    private $predavac;
+    private $poruka;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->predavac = new stdClass();
+        $this->predavac->sifra=0;
+        $this->predavac->ime='';
+        $this->predavac->prezime='';
+        $this->predavac->oib='';
+        $this->predavac->email='';
+        $this->predavac->iban='';
+    }
+
+
     public function index()
     {
-        $this->view->render($this->viewDir . 'index');
+       $this->view->render($this->viewDir . 'index',[
+           'predavaci'=>Predavac::read()
+       ]);
+    }   
+
+    public function detalji($sifra=0)
+    {
+        if($sifra===0){
+            $this->view->render($this->viewDir . 'detalji',[
+                'predavac'=>$this->predavac,
+                'poruka'=>'Unesite tražene podatke',
+                'akcija'=>'Dodaj novi'
+            ]);
+        }else{
+            $this->view->render($this->viewDir . 'detalji',[
+                'predavac'=>Predavac::readOne($sifra),
+                'poruka'=>'Promjenite podatke',
+                'akcija'=>'Promjena'
+            ]);
+        }
+
     }
+
+    public function akcija()
+    {
+        if($_POST['sifra']==0){
+            //prvo kontrole 
+            if($this->kontrolaOIB($_POST['oib'])){
+                Predavac::create($_POST);
+            }else{
+                $this->view->render($this->viewDir . 'detalji',[
+                    'predavac'=>(object)$_POST,
+                    'poruka'=>'Neispravan OIB',
+                    'akcija'=>'Dodaj novi'
+                ]);
+                return;
+            }
+            
+        }else{
+            //prvo kontrole 
+            Predavac::update($_POST);
+        }
+        header('location:' . App::config('url').'predavac/index');
+    }
+
+
+    public function brisanje($sifra)
+    {
+        Predavac::delete($sifra);
+        header('location:' . App::config('url').'predavac/index');
+    }
+
+
+
+    private function kontrolaOIB($oib) {
+
+        if (strlen($oib) != 11 || !is_numeric($oib)) {
+            return false;
+        }
+    
+        $a = 10;
+    
+        for ($i = 0; $i < 10; $i++) {
+    
+            $a += (int)$oib[$i];
+            $a %= 10;
+    
+            if ( $a == 0 ) { $a = 10; }
+    
+            $a *= 2;
+            $a %= 11;
+    
+        }
+    
+        $kontrolni = 11 - $a;
+    
+        if ( $kontrolni == 10 ) { $kontrolni = 0; }
+    
+        return $kontrolni == intval(substr($oib, 10, 1), 10);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function test($sto){
         switch ($sto) {
