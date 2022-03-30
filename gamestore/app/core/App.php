93b992bc98ab2464bcf9a0ce40b2e4c218c09ace
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-03-30 16:08:08 +0100

    class, instance, method

--- gamestore/app/core/App.php
+++ gamestore/app/core/App.php
@@ -8,6 +8,42 @@ class App
         //print_r($_SERVER);
         //echo '</pre>';
         $route = Request::getRoute();
-        echo $route;
+        //echo $route;
+
+        $parts = explode('/',$route);
+
+        //echo '<pre>';
+        //print_r($parts);
+        //echo '</pre>';
+
+        $class='';
+        if(!isset($parts[1]) || $parts[1]===''){
+            $class='Index';
+        }else{
+            $class=ucfirst($parts[1]);
+        }
+        $class .= 'Controller';
+        //echo $class;
+
+        $method='';
+        if(!isset($parts[2]) || $parts[2]===''){
+            $method='index';
+        }else{
+            $method=$parts[2];
+        }
+
+        //echo $class . '->' . $method . '();';
+
+        if(class_exists($class) && method_exists($class,$method)){
+            $instance = new $class();
+            $instance->$method();
+        }else{
+            //
+            echo $class . '->' . $method . '() does not exist';
+        }
+
+        //$controller = new IndexController();
+        //$controller->index();
+
     }
 }
