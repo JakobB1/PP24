0ee2efb7a1e46500b349d07e54c6696dc4c0babf
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-03-30 16:47:35 +0100

    Continue

--- gamestore/app/controller/PracticeController.php
+++ gamestore/app/controller/PracticeController.php
@@ -6,4 +6,24 @@ class PracticeController
     {
         echo 'Edunova';
     }
+
+    public function example2()
+    {
+        $sb = rand(2,9);
+        $name = 'Edunova';
+        $o = new stdClass();
+        $o->name='Pero';
+        $o->surname='Perić';
+        $row=[
+            'Osijek', 'Zagreb', 'Donji Miholjac'
+        ];
+
+        $view = new View();
+        $view->render('parameters',[
+            'randomNumber'=>$sb,
+            'school'=>$name,
+            'guide'=>$o,
+            'cities'=>$row
+        ]);
+    }
 }
