5669a45829d604d0d15be5a54b7e2053f91626ae
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-04-11 13:54:41 +0100

    Start od pocetka

---
+++ gamestore_hrv/app/model/App.php
@@ -0,0 +1,13 @@
+<?php
+
+class App 
+{
+    public static function start()
+    {
+        //echo '<pre>';
+        //print_r($_SERVER);
+        //echo '</pre>';
+        $ruta = Request::getRuta();
+        echo $ruta;
+    }
+}
