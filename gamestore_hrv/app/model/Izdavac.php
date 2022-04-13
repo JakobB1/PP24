e2c5ac0d00a4d595cee2594cbbbff2fc547f21ed
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-04-13 17:28:45 +0100

    Dodane akcije/ delete

--- gamestore_hrv/app/model/Izdavac.php
+++ gamestore_hrv/app/model/Izdavac.php
@@ -12,7 +12,11 @@ class Izdavac
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-            select * from izdavac
+            select i.sifra , i.naziv , i.drzava , i.webstranica, 
+            count(ig.sifra) as igre
+            from izdavac i left join igra ig
+            on i.sifra = ig.izdavac_id 
+            group by i.sifra , i.naziv , i.drzava , i.webstranica;
         
         '); 
         $izraz->execute();
