b0325d756037c57503ed4fa0989a65d3ee2ed79d
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-04-14 16:46:16 +0100

    Modeli, brisanje, dodavanje, uredivanje za razvijaca

--- gamestore_hrv/app/model/Razvijac.php
+++ gamestore_hrv/app/model/Razvijac.php
@@ -2,80 +2,69 @@
 
 class Razvijac
 {
-
-
-    // public static function readOne($kljuc)
-    // {
-    //     $veza = DB::getInstanca();
-    //     $izraz = $veza->prepare('
-        
-            
-    //         select r.sifra , r.naziv , r.drzava , r.webstranica, 
-    //         count (i.sifra) as igre
-    //         from razvijac r left join igra i 
-    //         on r.sifra = i.razvijac_id  
-    //         where r.sifra = :parametar
-    //         group by r.sifra , r.naziv , r.drzava , r.webstranica
-
-    //     '); 
-    //     $izraz->execute(['parametar'=>$kljuc]);
-    //     return $izraz->fetch();
-    // }
-
     // CRUD
+    public static function readOne($kljuc)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select * from razvijac where sifra=:parametar;
+        
+        '); 
+        $izraz->execute(['parametar'=>$kljuc]);
+        return $izraz->fetch();
+    }
 
-    //R - Read
+    // R - Read
     public static function read()
     {
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-            select r.sifra , r.naziv, r.drzava , r.webstranica, 
+            select r.sifra , r.naziv , r.drzava , r.webstranica, 
             count(ig.sifra) as igre
             from razvijac r left join igra ig
             on r.sifra = ig.razvijac_id 
             group by r.sifra , r.naziv , r.drzava , r.webstranica;
-        
         
         '); 
         $izraz->execute();
         return $izraz->fetchAll();
     }
 
-    //C - Create
-    // $parametri su asocijativni niz - tako mi odgovara
-    // public static function create($parametri)
-    // {
-    //     $veza = DB::getInstanca();
-    //     $izraz = $veza->prepare('
+    // C - Create
+    public static function create($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
         
-    //         insert into razvijac (naziv,drzava,webstranica)
-    //         values (:naziv,:drzava,:webstranica);
+            insert into razvijac (naziv,drzava,webstranica)
+            values (:naziv,:drzava,:webstranica);
         
-    //     '); 
-    //     $izraz->execute($parametri);
+        '); 
+        $izraz->execute($parametri);
         
-    // }
-    
+    }
 
     //U - Update
-    // public static function update($parametri)
-    // {
-    //     $veza = DB::getInstanca();
-    //     $izraz = $veza->prepare('
+    public static function update($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
         
-    //     update razvijac set 
-    //     naziv=:naziv,
-    //     drzava=:drzava,
-    //     webstranica=:webstranica
-    //     where sifra=:sifra;
+            update razvijac set 
+                naziv=:naziv,
+                drzava=:drzava,
+                webstranica=:webstranica
+                where sifra=:sifra;
         
-    //     '); 
-    //     $izraz->execute($parametri);
+        '); 
+        $izraz->execute($parametri);
         
-    // }
+    }
 
-    // //D - Delete
+    // D - Delete
+
     public static function delete($sifra)
     {
         $veza = DB::getInstanca();
@@ -87,6 +76,4 @@ class Razvijac
         $izraz->execute(['sifra'=>$sifra]);
 
     }
-
-    // }
 }
