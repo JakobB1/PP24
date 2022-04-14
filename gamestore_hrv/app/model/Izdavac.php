84c8bdb1bea490069dcf6e4227f05be1f36a027e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2022-04-14 16:46:03 +0100

    Modeli, brisanje, dodavanje, uredivanje za izdavaca

--- gamestore_hrv/app/model/Izdavac.php
+++ gamestore_hrv/app/model/Izdavac.php
@@ -3,8 +3,17 @@
 class Izdavac
 {
     // CRUD
-
-    // C - Create 
+    public static function readOne($kljuc)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select * from izdavac where sifra=:parametar;
+        
+        '); 
+        $izraz->execute(['parametar'=>$kljuc]);
+        return $izraz->fetch();
+    }
 
     // R - Read
     public static function read()
@@ -21,6 +30,37 @@ class Izdavac
         '); 
         $izraz->execute();
         return $izraz->fetchAll();
+    }
+
+    // C - Create
+    public static function create($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            insert into izdavac (naziv,drzava,webstranica)
+            values (:naziv,:drzava,:webstranica);
+        
+        '); 
+        $izraz->execute($parametri);
+        
+    }
+
+    //U - Update
+    public static function update($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            update izdavac set 
+                naziv=:naziv,
+                drzava=:drzava,
+                webstranica=:webstranica
+                where sifra=:sifra;
+        
+        '); 
+        $izraz->execute($parametri);
+        
     }
 
     // D - Delete
